749af5325a15701175892d4ca6c5e1fda1723610
Jesse Luoto <jesse.luoto@gmail.com>
2019-06-09 15:23:22 +0300

    Add eslint with standard.js linting
    Eslint also runs prettier behind the scenes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-const foo = 'bar'
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+const PUBLIC_DIR = path.join(__dirname, '../public')
+
+const parseUrl = request => {
+  const { dir, base } = path.parse(request.url)
+  return path.join(dir, base || 'index.html')
+}
+
+const server = http.createServer((request, response) => {
+  const targetFilePath = path.join(PUBLIC_DIR, parseUrl(request))
+  if (fs.existsSync(targetFilePath)) {
+    response.writeHead(200)
+    fs.createReadStream(targetFilePath).pipe(response)
+  } else {
+    response.writeHead(404)
+    response.end('Not found')
+  }
+})
+
+server.listen(process.env.PORT || 8000)
